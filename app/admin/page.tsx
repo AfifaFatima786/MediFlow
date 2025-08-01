83a0101e4d873e716d18f3ee188fdfea36539787
Afifa Fatima <afifarizvi7@gmail.com>
2025-08-01 18:01:10 +0530

    Dashboard Initialised

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import StatCard from '@/components/StatCard'
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14 text-white'>
+        <header className='admin-header'>
+            <Link href="/" className="cursor-pointer">
+
+            <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={162}
+            alt="logo"
+            classNameh-8 w-fit
+            
+            />
+
+            </Link>
+
+            <p className='text-16-semibold'>Admin Dashboard</p>
+
+        </header>
+
+        <main className='admin-main'>
+            <section className='w-full space-y-4'>
+                <h1 className='header'>Welcome 👋,Admin</h1>
+                <p className='text-dark-700'>Start the day with managing new appointments</p>
+
+            </section>
+
+
+            <section className='admin-start flex w-full justify-evenly gap-20'>
+
+                <StatCard
+                type="appointment"
+                count={5}
+                label="Scheduled appointments"
+                icon="/assets/icons/appointments.svg"
+                
+                />
+
+                <StatCard
+                type="pending"
+                count={10}
+                label="Pending appointments"
+                icon="/assets/icons/pending.svg"
+                
+                />
+
+                <StatCard
+                type="appointment"
+                count={2}
+                label="Cancelled appointments"
+                icon="/assets/icons/cancelled.svg"
+                
+                />
+
+            </section>
+
+        </main>
+
+
+
+    </div>
   )
 }
 
